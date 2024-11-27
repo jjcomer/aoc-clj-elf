0b36c25a489c1b0c12c58f3b7df7d32f92725074
Josh Comer <jcomer@cvent.com>
2024-11-26 20:52:21 -0400

    Move template into code

--- src/aoc_clj_elf/generate.clj
+++ src/aoc_clj_elf/generate.clj
@@ -4,10 +4,40 @@
             [clojure.string :as str] 
             [bling.core :as bling]))
 
+(def solution-template
+  "(ns y__YEAR__.d__DAY__
+  (:require [clojure.test :as t :refer [deftest]]))
+
+;; PROBLEM LINK https://adventofcode.com/__YEAR__/day/__DAY__
+
+;; Generator Logic
+
+;; Solution Logic
+
+;; Entry Points
+
+(defn generator
+  \"The generator fn is used to parse your input into. The output of this fn will be passed into each of the solving fns\"
+  [input])
+
+(defn solve-part-1
+  \"The solution to part 1. Will be called with the result of the generator\"
+  [input])
+
+(defn solve-part-2
+  \"The solution to part 2. Will be called with the result of the generator\"
+  [input])
+
+;; Tests
+;; Use tests to verify your solution. Consider using the sample data provided in the question
+
+(deftest sample-test
+  (t/is (= 2 (+ 1 1))))
+")
+
 (defn generate-solution
   [year day]
-  (let [solution-template (io/resource "solution.template")
-        safe-year (str "y" year)
+  (let [safe-year (str "y" year)
         safe-day (str "d" day)
         solution-filename (str safe-day ".clj")
         solution-path (fs/path "src" safe-year)
